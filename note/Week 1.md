d081912da0a3eb9bf8d0e7eee6f0e5248debaa03
Wei Lyu <lvwei1001@live.com>
2016-03-22 20:57:34 +0800

    done with three get methods in basicdocument

---
+++ note/Week 1.md
@@ -0,0 +1,12 @@
+- **regular expression**
+    - repetition
+        - **" +"** matches 1 or more spaces in a row, if find one, look for more
+    - repetition 
+        - **"it*"** means zero or more t
+    - alternation
+        - | means or
+    - character classes
+        - **"[123]"** mean match anything in the set
+    - excluding a character
+        - **"[^a-z123]"** ^ indicates NOT any characters in this set
+        
--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -1,93 +1,97 @@
 package document;
 
+import com.sun.javafx.fxml.expression.Expression;
+
+import java.util.ArrayList;
 import java.util.List;
 
-/** 
- * A naive implementation of the Document abstract class. 
+/**
+ * A naive implementation of the Document abstract class.
+ *
  * @author UC San Diego Intermediate Programming MOOC team
  */
-public class BasicDocument extends Document 
-{
-	/** Create a new BasicDocument object
-	 * 
-	 * @param text The full text of the Document.
-	 */
-	public BasicDocument(String text)
-	{
-		super(text);
-	}
-	
-	
-	/**
-	 * Get the number of words in the document.
-	 * "Words" are defined as contiguous strings of alphabetic characters
-	 * i.e. any upper or lower case characters a-z or A-Z
-	 * 
-	 * @return The number of words in the document.
-	 */
-	@Override
-	public int getNumWords()
-	{
-		//TODO: Implement this method.  See the Module 1 support videos 
-	    // if you need help.
-	    return 0;
-	}
-	
-	/**
-	 * Get the number of sentences in the document.
-	 * Sentences are defined as contiguous strings of characters ending in an 
-	 * end of sentence punctuation (. ! or ?) or the last contiguous set of 
-	 * characters in the document, even if they don't end with a punctuation mark.
-	 * 
-	 * @return The number of sentences in the document.
-	 */
-	@Override
-	public int getNumSentences()
-	{
-	    //TODO: Implement this method.  See the Module 1 support videos 
+public class BasicDocument extends Document {
+    /**
+     * Create a new BasicDocument object
+     *
+     * @param text The full text of the Document.
+     */
+    public BasicDocument(String text) {
+        super(text);
+    }
+
+
+    /**
+     * Get the number of words in the document.
+     * "Words" are defined as contiguous strings of alphabetic characters
+     * i.e. any upper or lower case characters a-z or A-Z
+     *
+     * @return The number of words in the document.
+     */
+    @Override
+    public int getNumWords() {
         // if you need help.
-        return 0;
-	}
-	
-	/**
-	 * Get the number of syllables in the document.
-	 * Words are defined as above.  Syllables are defined as:
-	 * a contiguous sequence of vowels, except for a lone "e" at the 
-	 * end of a word if the word has another set of contiguous vowels, 
-	 * makes up one syllable.   y is considered a vowel.
-	 * @return The number of syllables in the document.
-	 */
-	@Override
-	public int getNumSyllables()
-	{
-	    //TODO: Implement this method.  See the Module 1 support videos 
+        List<String> token = getTokens("[a-z]+");
+        return token.size();
+    }
+
+    /**
+     * Get the number of sentences in the document.
+     * Sentences are defined as contiguous strings of characters ending in an
+     * end of sentence punctuation (. ! or ?) or the last contiguous set of
+     * characters in the document, even if they don't end with a punctuation mark.
+     *
+     * @return The number of sentences in the document.
+     */
+    @Override
+    public int getNumSentences() {
         // if you need help.
-        return 0;
-	}
-	
-	
-	/* The main method for testing this class. 
-	 * You are encouraged to add your own tests.  */
-	public static void main(String[] args)
-	{
-		testCase(new BasicDocument("This is a test.  How many???  "
-		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
-				16, 13, 5);
-		testCase(new BasicDocument(""), 0, 0, 0);
-		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
-		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
-		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
-		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
-				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
-				+ "the correct amount of syllables (example, for example), "
-				+ "but most of them will."), 49, 33, 3);
-		testCase(new BasicDocument("Segue"), 2, 1, 1);
-		testCase(new BasicDocument("Sentence"), 2, 1, 1);
-		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
-		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
-		         32, 15, 1);
-		
-		
-	}
-	
+            List<String> token = getTokens("[^.!?]+");
+        return token.size();
+    }
+
+    /**
+     * Get the number of syllables in the document.
+     * Words are defined as above.  Syllables are defined as:
+     * a contiguous sequence of vowels, except for a lone "e" at the
+     * end of a word if the word has another set of contiguous vowels,
+     * makes up one syllable.   y is considered a vowel.
+     *
+     * @return The number of syllables in the document.
+     */
+    @Override
+    public int getNumSyllables() {
+        // if you need help.
+        List<String> tokens = getTokens("[a-zA-Z]");
+        int totalSyllables = 0;
+        for (String word : tokens) {
+            totalSyllables += countSyllables(word);
+        }
+        return totalSyllables;
+    }
+
+
+    /* The main method for testing this class.
+     * You are encouraged to add your own tests.  */
+    public static void main(String[] args) {
+        testCase(new BasicDocument("This is a test.  How many???  "
+                        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
+                16, 13, 5);
+        testCase(new BasicDocument(""), 0, 0, 0);
+        testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
+                + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
+        testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+        testCase(new BasicDocument("Here is a series of test sentences. Your program should "
+                + "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+                + "the correct amount of syllables (example, for example), "
+                + "but most of them will."), 49, 33, 3);
+        testCase(new BasicDocument("Segue"), 2, 1, 1);
+        testCase(new BasicDocument("Sentence"), 2, 1, 1);
+        testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+        testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
+                32, 15, 1);
+
+
+    }
+
 }
